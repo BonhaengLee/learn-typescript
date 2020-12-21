ff2321d360f296d353c947375b54ecede39ab2a5
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2020-12-21 23:44:52 +0900

    09Classes

---
+++ 09Classes/types.ts
@@ -0,0 +1,19 @@
+class Stark {
+    name : string = "Brandon";
+    static castle: string = "Winterfell"
+    saying: string;
+
+    constructor(){
+        this.saying = Stark.castle;
+    }
+
+    hello(person:string){
+        console.log("Hello"+person)
+    }
+}
+
+var ned = new Stark();
+//saying은 constructor assign castle 후 변화 없음
+ned.saying = "Winter is coming";
+//console.log(Stark.castle);
+ned.hello("Robert")
