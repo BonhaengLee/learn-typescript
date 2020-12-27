ca553b9411d881421b249f81bff47f263431a952
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2020-12-28 05:00:25 +0900

    201228 ts+redux 카운터 완성, eslint 재설정

---
+++ ts-react-redux-tutorial/src/containers/CounterContainer.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../modules/index";
+import { increase, decrease, increaseBy } from "../modules/counter";
+import Counter from "../components/Counter";
+
+export default function CounterContainer() {
+  // NOTE: ts에서 특별한 점은 useSelector 부분에서 state의 타입을 RootState로 지정해서 사용한다는 것 외에는 없다.
+  const count = useSelector((state: RootState) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => {
+    dispatch(increase());
+  };
+
+  const onDecrease = () => {
+    dispatch(decrease());
+  };
+
+  const onIncreaseBy = (diff: number) => {
+    dispatch(increaseBy(diff));
+  };
+
+  return (
+    <Counter
+      count={count}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+      onIncreaseBy={onIncreaseBy}
+    />
+  );
+}
--- ts-react-redux-tutorial/src/modules/counter.ts
+++ ts-react-redux-tutorial/src/modules/counter.ts
@@ -1,9 +1,9 @@
-// 액션 type 선언
+// NOTE: 액션 type 선언
 const INCREASE = "counter/INCREASE" as const;
 const DECREASE = "counter/DECREASE" as const;
 const INCREASE_BY = "counter/INCREASE_BY" as const;
 
-// 액션 생성 함수 선언
+// NOTE: 액션 생성 함수 선언
 // return 생략할 수 있어서 화살표 함수 이용
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
@@ -12,13 +12,13 @@ export const increaseBy = (diff: number) => ({
   payload: diff
 });
 
-// 액션 객체들에 대한 type 준비하기
+// NOTE: 액션 객체들에 대한 type 준비하기
 type CounterAction =
   | ReturnType<typeof increase>
   | ReturnType<typeof decrease>
   | ReturnType<typeof increaseBy>;
 
-// 상태의 타입과 상태의 초깃값 선언하기
+// NOTE: 상태의 타입과 상태의 초깃값 선언하기
 // 리덕스의 상태의 타입을 선언할 때는 type or interface
 type CounterState = {
   count: number;
@@ -28,7 +28,7 @@ const initialState: CounterState = {
   count: 0
 };
 
-// 리듀서 작성하기, useReducer와 비슷하다.
+// NOTE: 리듀서 작성하기, useReducer와 비슷하다.
 // 함수의 반환 타입에 상태의 타입을 넣는 것을 잊지 마라
 function counter(state: CounterState = initialState, action: CounterAction) {
   switch (action.type) {
