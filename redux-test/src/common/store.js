2e4982343fb8cb64f883d0d1ade221f77deaa691
BonhaengLee <bon952014@ajou.ac.kr>
2021-03-26 23:31:38 +0900

    210326 redux-saga in auth

--- redux-test/src/common/store.js
+++ redux-test/src/common/store.js
@@ -1,8 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import timelineReducer from "../timeline/state";
 import friendReducer from "../friend/state";
-import createSagaMiddleware from 'redux-saga'
-import timelineSaga from '../timeline/state/saga'
+import createSagaMiddleware from "redux-saga";
+import timelineSaga from "../timeline/state/saga";
 
 const reducer = combineReducers({
     timeline: timelineReducer,
@@ -10,7 +10,7 @@ const reducer = combineReducers({
 });
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 // @ : 스토어 객체를 원하는 곳에서 가져다 사용할 수 있음
 export default store;
-sagaMiddleware.run(timelineSaga)
+sagaMiddleware.run(timelineSaga);
