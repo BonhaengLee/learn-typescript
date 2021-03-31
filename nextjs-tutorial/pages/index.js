1f13c0ee4cacb3e1634fc51f7e732f768a14c1c4
BonhaengLee <bon952014@ajou.ac.kr>
2021-04-01 02:14:23 +0900

    210401 nextjs

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -2,7 +2,8 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
-import itemList from "../src/component/itemList";
+import ItemList from "../src/component/ItemList.js";
+import { Divider, Header } from "semantic-ui-react";
 
 export default function Home() {
     const [list, setList] = useState([]);
@@ -41,7 +42,16 @@ export default function Home() {
             <Head>
                 <title>HOME | sjwsne </title>
             </Head>
-            <itemList list={list} />
+            <Header as="h3" style={{ paddingTop: 40 }}>
+                베스트상품
+            </Header>
+            <Divider />
+            <ItemList list={list.slice(0, 9)} />
+            <Header as="h3" style={{ paddingTop: 40 }}>
+                신상품
+            </Header>
+            <Divider />
+            <ItemList list={list.slice(9)} />
         </div>
     );
 }
--- nextjs-tutorial/pages/view/[id].js
+++ nextjs-tutorial/pages/view/[id].js
@@ -1,5 +1,29 @@
-import React from "react";
+import Axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Item from "../../src/component/Item";
 
-export default function About() {
-    return <div>ID</div>;
-}
+const Post = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    const [item, setItem] = useState({});
+
+    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+
+    function getData() {
+        Axios.get(API_URL).then((res) => {
+            setItem(res.data);
+        });
+    }
+
+    useEffect(() => {
+        if (id && id > 0) {
+            getData();
+        }
+    }, [id]);
+
+    return <Item item={item} />;
+};
+
+export default Post;
