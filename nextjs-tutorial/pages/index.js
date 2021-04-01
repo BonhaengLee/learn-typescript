89b6286a16b181d1cfec92af4848fa1ec2af3429
BonhaengLee <bon952014@ajou.ac.kr>
2021-04-01 15:56:31 +0900

    210401 before applying SSR

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import ItemList from "../src/component/ItemList.js";
-import { Divider, Header } from "semantic-ui-react";
+import { Divider, Header, Loader } from "semantic-ui-react";
 
 export default function Home() {
     const [list, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const API_URL =
         "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
@@ -16,6 +17,7 @@ export default function Home() {
             .then((res) => {
                 console.log(res.data);
                 setList(res.data);
+                setIsLoading(false);
             })
             .catch(function (error) {
                 if (error.response) {
@@ -42,16 +44,28 @@ export default function Home() {
             <Head>
                 <title>HOME | sjwsne </title>
             </Head>
-            <Header as="h3" style={{ paddingTop: 40 }}>
-                베스트상품
-            </Header>
-            <Divider />
-            <ItemList list={list.slice(0, 9)} />
-            <Header as="h3" style={{ paddingTop: 40 }}>
-                신상품
-            </Header>
-            <Divider />
-            <ItemList list={list.slice(9)} />
+            <meta name="description" content="sdfasdf12e4124e123123"></meta>
+            {isLoading && (
+                <div style={{ padding: "300px 0" }}>
+                    <Loader inline="centered" active>
+                        Loading
+                    </Loader>
+                </div>
+            )}
+            {!isLoading && (
+                <>
+                    <Header as="h3" style={{ paddingTop: 40 }}>
+                        베스트상품
+                    </Header>
+                    <Divider />
+                    <ItemList list={list.slice(0, 9)} />
+                    <Header as="h3" style={{ paddingTop: 40 }}>
+                        신상품
+                    </Header>
+                    <Divider />
+                    <ItemList list={list.slice(9)} />
+                </>
+            )}
         </div>
     );
 }
